ab9556b2c4439a2c01b1892dc26cd539cec36064
EC2 Default User <ec2-user@ip-172-31-35-105.us-east-2.compute.internal>
2023-03-27 04:13:28 +0000

    Update

--- class2/main.tf
+++ class2/main.tf
@@ -1,5 +1,69 @@
-resource "aws_instance" "web" {
-  ami           = "ami-00c39f71452c08778"
-  instance_type = "t3.micro"
-
+resource "aws_key_pair" "class2" {
+  key_name   = "class2-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
+
+# creates sec group
+resource "aws_security_group" "class2" {
+  name        = "class2"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+# creates an EBS Volume 
+resource "aws_ebs_volume" "class2" {
+  availability_zone = "us-east-1a"
+  size              = 40
+}
+
+
+
+resource "aws_instance" "web" {
+  ami                         = "ami-00c39f71452c08778"
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  availability_zone           = "us-east-1a"
+  key_name = aws_key_pair.class2.key_name
+  vpc_security_group_ids = [aws_security_group.class2.id]
+}
+
+ # attaches volume to an instance
+resource "aws_volume_attachment" "class2" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.class2.id
+  instance_id = aws_instance.web.id
+}
+
