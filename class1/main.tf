8b206553a4611f30391060f58eeed221b73e67b1
EC2 Default User <ec2-user@ip-172-31-35-105.us-east-2.compute.internal>
2023-03-27 02:40:53 +0000

    Update file

---
+++ class1/main.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_user" "firstuser" {
+  for_each = toset([
+    "firstuser",
+    "seconduser",
+    "thirduser",
+  ])
+  name = each.key
+}
+resource "aws_iam_group" "multigroup" {
+  for_each = toset([
+    "sales",
+    "marketing",
+    "develop",
+  ])
+  name = each.key
+}
+
+resource "aws_key_pair" "first" {
+  key_name   = "first-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket_prefix = "my-"
+}
