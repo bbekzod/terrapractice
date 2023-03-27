9f9354489371cab9bb7a76d7991635aa8fab1f0a
EC2 Default User <ec2-user@ip-172-31-35-105.us-east-2.compute.internal>
2023-03-27 04:52:13 +0000

    Update

--- class2/main.tf
+++ class2/main.tf
@@ -1,3 +1,4 @@
+# creates a key pair
 resource "aws_key_pair" "class2" {
   key_name   = "class2-key"
   public_key = file("~/.ssh/id_rsa.pub")
@@ -42,7 +43,6 @@ resource "aws_security_group" "class2" {
   }
 }
 
-
 # creates an EBS Volume 
 resource "aws_ebs_volume" "class2" {
   availability_zone = "us-east-1a"
@@ -50,20 +50,57 @@ resource "aws_ebs_volume" "class2" {
 }
 
 
-
+# creates ec2 instance
 resource "aws_instance" "web" {
-  ami                         = "ami-00c39f71452c08778"
+  ami                         = "ami-02f3f602d23f1659d"
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   availability_zone           = "us-east-1a"
-  key_name = aws_key_pair.class2.key_name
-  vpc_security_group_ids = [aws_security_group.class2.id]
+  key_name                    = aws_key_pair.class2.key_name
+  user_data = file("wordpress_install.sh")
+  vpc_security_group_ids = [
+    aws_security_group.class2.id,
+  ]
 }
 
- # attaches volume to an instance
-resource "aws_volume_attachment" "class2" {
-  device_name = "/dev/sdb"
-  volume_id   = aws_ebs_volume.class2.id
-  instance_id = aws_instance.web.id
+# creates ec2 instance
+resource "aws_instance" "web2" {
+  ami                         = "ami-02f3f602d23f1659d"
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.class2.key_name
+  vpc_security_group_ids = [
+    aws_security_group.class2.id,
+  ]
 }
 
+# terraform import aws_instance.mine    i-0efeb1db621f7809d
+resource "aws_instance" "mine" {
+  ami = "ami-02f3f602d23f1659d"
+  instance_type = "t3.micro"
+}
+
+
+
+
+# # attaches volume to an instance
+# resource "aws_volume_attachment" "class2" {
+#   device_name = "/dev/sdb"
+#   volume_id   = aws_ebs_volume.class2.id
+#   instance_id = aws_instance.web.id
+# }
+
+
+# # creates DNS Record
+# resource "aws_route53_record" "class2" {
+#   allow_overwrite = true
+#   zone_id         = var.zone_id
+#   name            = "blog.${var.domain}"
+#   type            = "A"
+#   ttl             = 300
+#   records         = [aws_instance.web.public_ip]
+# }
+
+# variable "zone_id" {}
+# variable "domain" {}
